132d3cf6b748639cd7916e93190a1c80c671abd2
linggash <sempanalingga@gmail.com>
2023-02-27 14:54:20 +0700

    Explicit Intent Practice Finished

--- app/src/main/java/com/linggash/myintentapp/MainActivity.kt
+++ app/src/main/java/com/linggash/myintentapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.linggash.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
